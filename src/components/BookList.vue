87c628890cb56ab9390c29b29fe13215875e4088
ksh <seongs70@naver.com>
2020-06-09 16:53:51 +0900

    first commit

---
+++ src/components/BookList.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "BookList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ShoppingCart"
+    }
+</script>
+
+<style scoped>
+
+</style>
